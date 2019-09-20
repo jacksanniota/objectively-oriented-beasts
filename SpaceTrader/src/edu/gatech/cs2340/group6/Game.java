4cccb6bc90ca6f5fba6933dfcab446bedbe67163
Matthew Marion <matthewjmarion29@gmail.com>
2019-09-19 21:37:43 -0400

    Menu screen and structure for new screens

--- SpaceTrader/src/edu/gatech/cs2340/group6/Game.java
+++ SpaceTrader/src/edu/gatech/cs2340/group6/Game.java
@@ -1,4 +1,50 @@
 package edu.gatech.cs2340.group6;
 
-public class Game {
+import edu.gatech.cs2340.group6.screen.ConfigScreen;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Game extends JFrame {
+
+    public static Game instance = null;
+
+    public Game(String title) {
+        this.setTitle(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        JPanel contentPane = createStartPanel();
+        this.setContentPane(contentPane);
+        this.setVisible(true);
+    }
+
+    private JPanel createStartPanel() {
+        JPanel jPanel = new JPanel(new BorderLayout());
+        JLabel welcomeText = new JLabel("Welcome to SpaceTrader");
+        welcomeText.setHorizontalAlignment(JLabel.CENTER);
+        JButton button = new JButton("Start Game");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                instance.getContentPane().removeAll();
+                instance.getContentPane().add(new ConfigScreen().getScreen());
+                instance.getContentPane().validate();
+                instance.getContentPane().repaint();
+            }
+        });
+        jPanel.add(welcomeText, BorderLayout.CENTER);
+        jPanel.add(button, BorderLayout.SOUTH);
+        return jPanel;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Game game = new Game("SpaceTrader");
+        instance = game;
+    }
+
 }
---
+++ SpaceTrader/src/edu/gatech/cs2340/group6/screen/ConfigScreen.java
@@ -0,0 +1,12 @@
+package edu.gatech.cs2340.group6.screen;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ConfigScreen {
+
+    public JPanel getScreen() {
+        JPanel jPanel = new JPanel(new BorderLayout());
+        return jPanel;
+    }
+}
