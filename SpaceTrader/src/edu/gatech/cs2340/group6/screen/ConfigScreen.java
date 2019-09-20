4dbbcb23d9d887190c84b80d2e263ba0b4bce424
Matthew Marion <matthewjmarion29@gmail.com>
2019-09-19 21:49:28 -0400

    Config screen item work

--- SpaceTrader/src/edu/gatech/cs2340/group6/screen/ConfigScreen.java
+++ SpaceTrader/src/edu/gatech/cs2340/group6/screen/ConfigScreen.java
@@ -7,6 +7,15 @@ public class ConfigScreen {
 
     public JPanel getScreen() {
         JPanel jPanel = new JPanel(new BorderLayout());
+        JLabel nameLabel = new JLabel("Name");
+        JTextField nameInput = new JTextField("John Smith");
+        nameInput.setSize(100, 20);
+        JLabel selectDifficulty = new JLabel("Select Difficulty");
+        JRadioButton easyButton = new JRadioButton("Easy");
+        JRadioButton mediumButton = new JRadioButton("Medium");
+        JRadioButton hardButton = new JRadioButton("Hard");
+        jPanel.add(nameLabel, BorderLayout.WEST);
+        jPanel.add(nameInput, BorderLayout.NORTH);
         return jPanel;
     }
 }
